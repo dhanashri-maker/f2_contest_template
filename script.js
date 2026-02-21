4c068fa105649363817e6f6d6021c44dd6012dca
dhanashri-maker <thedhanashrishinde@gmail.com>
2026-02-21 23:06:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,60 @@
-/** @format */
-
-let arr = [
-  { id: 1, name: "john", age: "18", profession: "developer" },
-  { id: 2, name: "jack", age: "20", profession: "developer" },
-  { id: 3, name: "karen", age: "19", profession: "admin" },
+// Given Array
+let employees = [
+  {id:1, name:"john", age:"18", profession:"developer"},
+  {id:2, name:"jack", age:"20", profession:"developer"},
+  {id:3, name:"karen", age:"19", profession:"admin"}
 ];
 
+//  Print developers using map()
 function PrintDeveloperbyMap() {
-  //Write your code here , just console.log
+  employees.map(function(emp) {
+    if(emp.profession === "developer") {
+      console.log(emp);
+    }
+  });
 }
 
+//  Print developers using forEach()
 function PrintDeveloperbyForEach() {
-  //Write your code here , just console.log
+  employees.forEach(function(emp) {
+    if(emp.profession === "developer") {
+      console.log(emp);
+    }
+  });
 }
 
+//  Add new employee data
 function addData() {
-  //Write your code here, just console.log
+  let newEmployee = {
+    id:4,
+    name:"susan",
+    age:"20",
+    profession:"intern"
+  };
+
+  employees.push(newEmployee);
+  console.log(employees);
 }
 
+//  Remove admin employees
 function removeAdmin() {
-  //Write your code here, just console.log
+  employees = employees.filter(function(emp) {
+    return emp.profession !== "admin";
+  });
+
+  console.log(employees);
 }
 
-function concatenateArray() {
-  //Write your code here, just console.log
+//  Concatenate another array
+function ConcatinateArray() {
+
+  let newEmployees = [
+    {id:5, name:"mike", age:"22", profession:"designer"},
+    {id:6, name:"emma", age:"23", profession:"tester"},
+    {id:7, name:"robert", age:"21", profession:"manager"}
+  ];
+
+  let combinedArray = employees.concat(newEmployees);
+
+  console.log(combinedArray);
 }
